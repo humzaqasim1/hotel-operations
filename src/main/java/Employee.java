2687325c4943585e8249728473df3833acb04d30
humzaqasim1 <humzaqasim1@gmail.com>
2025-10-21 13:58:39 -0500

    Made get total pay method

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,12 +1,54 @@
 public class Employee {
-    private int employeeId;
+    private String employeeId;
     private String name;
     private String department;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
     private double payRate;
     private int hoursWorked;
 
-    public void getTotalPay(){
-
+    public double getTotalPay(){
+        double totalPay = hoursWorked * payRate;
+        return totalPay;
     }
     public void getRegularHours(){
 
